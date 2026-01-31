e3d85130ad87430bdbbfba71356a4dc7f1395a99
ritvik <ritviksny@gmail.com>
2026-01-31 23:16:06 +0530

    Started the Streamlit Application

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -1,3 +1,19 @@
 import streamlit as st
+import pandas as pd
+from pathlib import Path
 
-print("Streamlit App")
+from src.preprocessing import (
+    load_data,
+    clean_data,
+    get_train_test_data,
+    build_preprocessor
+)
+
+from src.model import train_xgboost
+from src.shap_utils import (
+    create_shap_explainer,
+    explain_single_customer,
+    get_top_features
+)
+
+from src.recommendation_engine import generate_precautions
